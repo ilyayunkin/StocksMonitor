64abb83d9ef759791c7f5c74035ce06843514f72
ilyayunkin <ilya.yunkin@ya.ru>
2020-10-10 16:21:49 +0600

    Deletion dialog realized

--- src/WidgetsUi/Dialogs.cpp
+++ src/WidgetsUi/Dialogs.cpp
@@ -11,10 +11,10 @@ Dialogs::Dialogs(QWidget * const w) :
 bool Dialogs::askReplaceBuyRequest(const QByteArray &ticker, const float sellPrice)
 {
     return
-        QMessageBox::question(w,QObject::tr("Replace?"),
-        QObject::tr("There already is %1:\n %1 %2")
-        .arg(QString(ticker))
-        .arg(sellPrice))
+            QMessageBox::question(w,QObject::tr("Replace?"),
+                                  QObject::tr("There already is %1:\n %1 %2")
+                                  .arg(QString(ticker))
+                                  .arg(sellPrice))
             == QMessageBox::Yes;
 }
 
@@ -33,5 +33,17 @@ bool Dialogs::askAddQuantityToPortfolio(const QByteArray &ticker)
                                  QObject::tr("Add?"),
                                  QObject::tr("There already is %1:\n %1")
                                  .arg(QString(ticker)))
-           == QMessageBox::Yes;
+            == QMessageBox::Yes;
+}
+
+bool Dialogs::askBuyRequestDoubleDeletion(const QByteArray &ticker1, const float basePrice1,
+                                          const QByteArray &ticker2, const float basePrice2)
+{
+    return QMessageBox::question(0, QObject::tr("Delete double?"),
+                                 QString("%1 %2\n%3 %4")
+                                 .arg(QString(ticker1))
+                                 .arg(basePrice1)
+                                 .arg(QString(ticker2))
+                                 .arg(basePrice2))
+            == QMessageBox::Yes;
 }
--- src/WidgetsUi/Dialogs.h
+++ src/WidgetsUi/Dialogs.h
@@ -12,6 +12,7 @@ public:
     bool askReplaceBuyRequest(const QByteArray &ticker, const float sellPrice) override;
     bool askDeleteFromPortfolio(const QByteArray &ticker) override;
     bool askAddQuantityToPortfolio(const QByteArray &ticker) override;
+    bool askBuyRequestDoubleDeletion(const QByteArray &ticker1, const float basePrice1, const QByteArray &ticker2, const float basePrice2);
 };
 
 #endif // DIALOGS_H
