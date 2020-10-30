8c1f5f67415fd7304eaa69bd4e7072e8c765502f
ilyayunkin <ilya.yunkin@ya.ru>
2020-10-30 13:56:45 +0600

    Refactoring: in StatisticsWidget long lambdas extracted into private functions, private members' declarations moved to the botom of the class definition

--- src/WidgetsUi/StatisticsWidget.cpp
+++ src/WidgetsUi/StatisticsWidget.cpp
@@ -7,186 +7,33 @@
 #include <QDebug>
 #include <QInputDialog>
 
-void StatisticsWidget::updateStocksList(const QString &pluginSelected)
-{
-    qDebug() << __PRETTY_FUNCTION__ << __LINE__ << pluginSelected;
-    ui->stocksComboBox->clear();
-    auto stocks = this->application.getStockIdList(pluginSelected);
-    if(!stocks.empty())
-    {
-        auto comparator = [](auto const &lhs, auto const &rhs)
-        {
-            return lhs.name < rhs.name;
-        };
-        std::sort(stocks.begin(), stocks.end(), comparator);
-
-        const auto addId = [this](const auto &id)
-        {
-            ui->stocksComboBox->addItem(
-                        QString("%1(%2)")
-                        .arg(id.name)
-                        .arg(id.ticker.data())
-                        , id.ticker.data());
-        };
-
-        std::for_each(stocks.begin(), stocks.end(), addId);
-    }else
-    {
-        ui->stocksComboBox->addItem(tr("No items - try later"));
-    }
-}
-
 StatisticsWidget::StatisticsWidget(Application &application, QWidget *parent)
     : QWidget(parent)
+    , ui(new Ui::StatisticsWidget)
     , application(application)
     , configController(application.getStatisticsController())
     , model(new StatisticsModel(configController.getStatisticsConfig(), this))
-    , ui(new Ui::StatisticsWidget)
 {
     ui->setupUi(this);
     ui->pluginsComboBox->addItems(application.getPluginsList());
     ui->treeView->setModel(model);
     ui->treeView->expandAll();
-    application.setStatisticsConfigView(this);
-    auto updateEnables = [this](const QModelIndex &index)
-    {
-        auto type = model->getType(index);
-        switch (type) {
-        case StatisticsModelElementType::ROOT:
-            ui->addButton->setEnabled(true);
-            ui->removeButton->setEnabled(false);
-            ui->pluginsComboBox->setEnabled(false);
-            ui->stocksComboBox->setEnabled(false);
-            break;
-        case StatisticsModelElementType::CATEGORY:
-            ui->addButton->setEnabled(true);
-            ui->removeButton->setEnabled(true);
-            ui->pluginsComboBox->setEnabled(false);
-            ui->stocksComboBox->setEnabled(false);
-            break;
-        case StatisticsModelElementType::GROUP:
-            ui->addButton->setEnabled(true);
-            ui->removeButton->setEnabled(true);
-            ui->pluginsComboBox->setEnabled(true);
-            ui->stocksComboBox->setEnabled(true);
-            break;
-        case StatisticsModelElementType::STOCK:
-            ui->addButton->setEnabled(false);
-            ui->removeButton->setEnabled(true);
-            ui->pluginsComboBox->setEnabled(true);
-            ui->stocksComboBox->setEnabled(true);
-            break;
-        default:
-        case StatisticsModelElementType::NONE:
-            ui->addButton->setEnabled(false);
-            ui->removeButton->setEnabled(false);
-            ui->pluginsComboBox->setEnabled(false);
-            ui->stocksComboBox->setEnabled(false);
-            break;
-        }
-    };
-    connect(ui->treeView, &QTreeView::clicked, updateEnables);
 
+    application.setStatisticsConfigView(this);
+
+    connect(ui->treeView, &QTreeView::clicked
+            , this, &StatisticsWidget::updateEnables);
     // Here should be QComboBox::textActivated, which is introduced in the Qt 5.14
     // But Travis CI uses 15.12 currently.
-    connect(ui->pluginsComboBox, QOverload<const QString &>::of(&QComboBox::activated),
-            this, &StatisticsWidget::updateStocksList);
-
+    connect(ui->pluginsComboBox, QOverload<const QString &>::of(&QComboBox::activated)
+            , this, &StatisticsWidget::updateStocksList);
     connect(ui->processButton, &QPushButton::clicked,
             [&application](){application.getStatisticsController().processStatistics();});
+    connect(ui->addButton, &QPushButton::clicked
+            , this, &StatisticsWidget::addStatisticsConfigElement);
+    connect(ui->removeButton, &QPushButton::clicked
+            , this, &StatisticsWidget::removeSelectedConfigTreeElement);
 
-    auto add = [this]
-    {
-        auto index = ui->treeView->selectionModel()->currentIndex();
-        auto type = model->getType(index);
-        auto configElement = model->getTreeElement(index);
-        switch (type) {
-        case StatisticsModelElementType::ROOT:
-        {
-            const auto categoryName = QInputDialog::getText(this, tr("Input category name"), tr("Input category name"));
-            if(!categoryName.isEmpty())
-            {
-                StatisticsConfigCategoryIndex catIndex(categoryName, static_cast<void *>(configElement));
-                configController.addCategory(catIndex);
-            }
-        }
-            break;
-        case StatisticsModelElementType::CATEGORY:
-        {
-            const auto groupName = QInputDialog::getText(this, tr("Input group name"), tr("Input group name"));
-            if(!groupName.isEmpty())
-            {
-                StatisticsConfigGroupIndex groupIndex(configElement->getName()
-                                                      , groupName
-                                                      , static_cast<void *>(configElement));
-                configController.addGroup(groupIndex);
-            }
-        }
-            break;
-        case StatisticsModelElementType::GROUP:
-        {
-            const auto ticker = ui->stocksComboBox->itemData(ui->stocksComboBox->currentIndex()).toByteArray();
-            if(!ticker.isEmpty())
-            {
-                const auto name = ui->stocksComboBox->currentText();
-                StatisticsConfigItemIndex itemIndex(configElement->getParent()->getName()
-                                                    , configElement->getName()
-                                                    , StockId{ticker.data(), name}
-                                                    , static_cast<void *>(configElement));
-                configController.addItem(itemIndex);
-            }
-            break;
-        }
-        default:
-        case StatisticsModelElementType::STOCK:
-        case StatisticsModelElementType::NONE:
-            break;
-        }
-
-    };
-    connect(ui->addButton, &QPushButton::clicked, add);
-    auto remove = [this]
-    {
-        auto index = ui->treeView->selectionModel()->currentIndex();
-        auto type = model->getType(index);
-        auto configElement = model->getTreeElement(index);
-        auto parentElement = configElement->getParent();
-        switch (type) {
-        case StatisticsModelElementType::CATEGORY:
-        {
-            StatisticsConfigCategoryIndex catIndex(configElement->getName()
-                                                   , static_cast<void *>(configElement));
-            configController.removeCategory(catIndex);
-        }
-            break;
-        case StatisticsModelElementType::GROUP:
-        {
-            StatisticsConfigGroupIndex groupIndex(parentElement->getName()
-                                                  , configElement->getName()
-                                                  , static_cast<void *>(configElement));
-            configController.removeGroup(groupIndex);
-        }
-            break;
-        case StatisticsModelElementType::STOCK:
-        {
-            const auto ticker = configElement->getTicker();
-            assert(!ticker.isEmpty());
-
-            const auto name = configElement->getName();
-            StatisticsConfigItemIndex itemIndex(configElement->getParent()->getParent()->getName()
-                                                , configElement->getParent()->getName()
-                                                , StockId{ticker.data(), name}
-                                                , static_cast<void *>(configElement));
-            configController.removeItem(itemIndex);
-            break;
-        }
-        default:
-        case StatisticsModelElementType::ROOT:
-        case StatisticsModelElementType::NONE:
-            break;
-        }
-    };
-    connect(ui->removeButton, &QPushButton::clicked, remove);
     updateStocksList(ui->stocksComboBox->currentText());
 }
 
@@ -229,3 +76,161 @@ void StatisticsWidget::removeCategory(const StatisticsConfigCategoryIndex &index
     auto element = static_cast<StatisticsTreeElement *>(index.internalPointer);
     model->remove(element);
 }
+
+void StatisticsWidget::updateStocksList(const QString &pluginSelected)
+{
+    qDebug() << __PRETTY_FUNCTION__ << __LINE__ << pluginSelected;
+    ui->stocksComboBox->clear();
+    auto stocks = this->application.getStockIdList(pluginSelected);
+    if(!stocks.empty())
+    {
+        auto comparator = [](auto const &lhs, auto const &rhs)
+        {
+            return lhs.name < rhs.name;
+        };
+        std::sort(stocks.begin(), stocks.end(), comparator);
+
+        const auto addId = [this](const auto &id)
+        {
+            ui->stocksComboBox->addItem(
+                        QString("%1(%2)")
+                        .arg(id.name)
+                        .arg(id.ticker.data())
+                        , id.ticker.data());
+        };
+
+        std::for_each(stocks.begin(), stocks.end(), addId);
+    }else
+    {
+        ui->stocksComboBox->addItem(tr("No items - try later"));
+    }
+}
+
+void StatisticsWidget::updateEnables(const QModelIndex &index)
+{
+    auto type = model->getType(index);
+    switch (type) {
+    case StatisticsModelElementType::ROOT:
+        ui->addButton->setEnabled(true);
+        ui->removeButton->setEnabled(false);
+        ui->pluginsComboBox->setEnabled(false);
+        ui->stocksComboBox->setEnabled(false);
+        break;
+    case StatisticsModelElementType::CATEGORY:
+        ui->addButton->setEnabled(true);
+        ui->removeButton->setEnabled(true);
+        ui->pluginsComboBox->setEnabled(false);
+        ui->stocksComboBox->setEnabled(false);
+        break;
+    case StatisticsModelElementType::GROUP:
+        ui->addButton->setEnabled(true);
+        ui->removeButton->setEnabled(true);
+        ui->pluginsComboBox->setEnabled(true);
+        ui->stocksComboBox->setEnabled(true);
+        break;
+    case StatisticsModelElementType::STOCK:
+        ui->addButton->setEnabled(false);
+        ui->removeButton->setEnabled(true);
+        ui->pluginsComboBox->setEnabled(true);
+        ui->stocksComboBox->setEnabled(true);
+        break;
+    default:
+    case StatisticsModelElementType::NONE:
+        ui->addButton->setEnabled(false);
+        ui->removeButton->setEnabled(false);
+        ui->pluginsComboBox->setEnabled(false);
+        ui->stocksComboBox->setEnabled(false);
+        break;
+    }
+}
+
+void StatisticsWidget::addStatisticsConfigElement()
+{
+    auto index = ui->treeView->selectionModel()->currentIndex();
+    auto type = model->getType(index);
+    auto configElement = model->getTreeElement(index);
+    switch (type) {
+    case StatisticsModelElementType::ROOT:
+    {
+        const auto categoryName = QInputDialog::getText(this, tr("Input category name"), tr("Input category name"));
+        if(!categoryName.isEmpty())
+        {
+            StatisticsConfigCategoryIndex catIndex(categoryName, static_cast<void *>(configElement));
+            configController.addCategory(catIndex);
+        }
+    }
+        break;
+    case StatisticsModelElementType::CATEGORY:
+    {
+        const auto groupName = QInputDialog::getText(this, tr("Input group name"), tr("Input group name"));
+        if(!groupName.isEmpty())
+        {
+            StatisticsConfigGroupIndex groupIndex(configElement->getName()
+                                                  , groupName
+                                                  , static_cast<void *>(configElement));
+            configController.addGroup(groupIndex);
+        }
+    }
+        break;
+    case StatisticsModelElementType::GROUP:
+    {
+        const auto ticker = ui->stocksComboBox->itemData(ui->stocksComboBox->currentIndex()).toByteArray();
+        if(!ticker.isEmpty())
+        {
+            const auto name = ui->stocksComboBox->currentText();
+            StatisticsConfigItemIndex itemIndex(configElement->getParent()->getName()
+                                                , configElement->getName()
+                                                , StockId{ticker.data(), name}
+                                                , static_cast<void *>(configElement));
+            configController.addItem(itemIndex);
+        }
+        break;
+    }
+    default:
+    case StatisticsModelElementType::STOCK:
+    case StatisticsModelElementType::NONE:
+        break;
+    }
+}
+
+void StatisticsWidget::removeSelectedConfigTreeElement()
+{
+    auto index = ui->treeView->selectionModel()->currentIndex();
+    auto type = model->getType(index);
+    auto configElement = model->getTreeElement(index);
+    auto parentElement = configElement->getParent();
+    switch (type) {
+    case StatisticsModelElementType::CATEGORY:
+    {
+        StatisticsConfigCategoryIndex catIndex(configElement->getName()
+                                               , static_cast<void *>(configElement));
+        configController.removeCategory(catIndex);
+    }
+        break;
+    case StatisticsModelElementType::GROUP:
+    {
+        StatisticsConfigGroupIndex groupIndex(parentElement->getName()
+                                              , configElement->getName()
+                                              , static_cast<void *>(configElement));
+        configController.removeGroup(groupIndex);
+    }
+        break;
+    case StatisticsModelElementType::STOCK:
+    {
+        const auto ticker = configElement->getTicker();
+        assert(!ticker.isEmpty());
+
+        const auto name = configElement->getName();
+        StatisticsConfigItemIndex itemIndex(configElement->getParent()->getParent()->getName()
+                                            , configElement->getParent()->getName()
+                                            , StockId{ticker.data(), name}
+                                            , static_cast<void *>(configElement));
+        configController.removeItem(itemIndex);
+        break;
+    }
+    default:
+    case StatisticsModelElementType::ROOT:
+    case StatisticsModelElementType::NONE:
+        break;
+    }
+};
--- src/WidgetsUi/StatisticsWidget.h
+++ src/WidgetsUi/StatisticsWidget.h
@@ -13,27 +13,30 @@ class StatisticsModel;
 class StatisticsWidget : public QWidget, public AbstractStatisticsConfigView
 {
     Q_OBJECT
-
-    Application &application;
-    StatisticsController &configController;
-    StatisticsModel *model = nullptr;
-    void updateStocksList(const QString &pluginSelected);
 public:
     explicit StatisticsWidget(Application &application,
                               QWidget *parent = nullptr);
     ~StatisticsWidget();
 
-private:
-    Ui::StatisticsWidget *ui;
-
     // AbstractStatisticsConfigView interface
-public:
     void addItem(const StatisticsConfigItemIndex &index) override;
     void addGroup(const StatisticsConfigGroupIndex &index) override;
     void addCategory(const StatisticsConfigCategoryIndex &index) override;
     void removeItem(const StatisticsConfigItemIndex &index) override;
     void removeGroup(const StatisticsConfigGroupIndex &index) override;
     void removeCategory(const StatisticsConfigCategoryIndex &index) override;
+
+private:
+    Ui::StatisticsWidget *ui;
+
+    Application &application;
+    StatisticsController &configController;
+    StatisticsModel *model = nullptr;
+
+    void updateStocksList(const QString &pluginSelected);
+    void updateEnables(const QModelIndex &index);
+    void addStatisticsConfigElement();
+    void removeSelectedConfigTreeElement();
 };
 
 #endif // STATISTICSWIDGET_H
