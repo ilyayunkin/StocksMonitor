a8c9a03b724cf58fa3f64128b8e7e028804718d4
ilyayunkin <ilya.yunkin@ya.ru>
2021-02-16 12:01:02 +0600

    CBRF URL building fixed for one digit dates

--- CbrfMetalPlugin/CbrfMetalPlugin.cpp
+++ CbrfMetalPlugin/CbrfMetalPlugin.cpp
@@ -6,11 +6,11 @@ QUrl CbrfMetalPlugin::getUrl() const
     QDate current = QDate::currentDate();
     QDate yesterday = QDate::currentDate().addDays(-1);
     auto url = QString(R"URL(http://www.cbr.ru/scripts/xml_metall.asp?date_req1=%1/%2/%3&date_req2=%4/%5/%6)URL")
-            .arg(yesterday.day())
-            .arg(yesterday.month())
+            .arg(yesterday.day(), 2, 10, QChar('0'))
+            .arg(yesterday.month(), 2, 10, QChar('0'))
             .arg(yesterday.year())
-            .arg(current.day())
-            .arg(current.month())
+            .arg(current.day(), 2, 10, QChar('0'))
+            .arg(current.month(), 2, 10, QChar('0'))
             .arg(current.year());
     return QUrl(url);
 }
